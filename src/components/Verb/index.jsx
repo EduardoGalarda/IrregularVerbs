8bbf48c54c663d2b737c752d90a67e3531f70443
Eduardo Manasses <116527687+EduardoGalarda@users.noreply.github.com>
2024-07-12 12:58:41 -0400

    update

--- src/components/Verb/index.jsx
+++ src/components/Verb/index.jsx
@@ -1,53 +1,95 @@
 import styles from './Verbs.module.css'
+import { GrEdit, GrPlay } from "react-icons/gr";
 
 const Verb = ({ verb, showDetails, onToggle }) => {
   return (
-    <div>
 
+    <div>
       <div className={styles.container}>
-            <ul className={styles.table}>
-                
-                    <li key={verb.id} className={styles.row}>
-                        <div className={styles.cell}>{verb.infinitive}</div>
-                        <div className={styles.cell}>{verb.simple_past}</div>
-                        <div className={styles.cell}>{verb.past_participle}</div>
-                        <div className={styles.cell}>{verb.translate}</div>
-                    </li>
-                
-            </ul>
-            <button className={styles.button} onClick={onToggle}> {showDetails ? "Fechar" : "Mostrar Detalhes"} </button>
-        </div>
+        <ul className={styles.table}>                
+          <li key={verb.id} className={styles.row}>
+            <div className={styles.cell}>{verb.infinitive}</div>
+            <div className={styles.cell}>{verb.simple_past}</div>
+            <div className={styles.cell}>{verb.past_participle}</div>
+            <div className={styles.cell}>{verb.translate}</div>
+          </li>                
+        </ul>
+        <button className={styles.button} onClick={onToggle}> {showDetails ? "Fechar" : "Mostrar Detalhes"} </button>
+      </div>
         
-      {/* <h3>{verb.infinitive}</h3> */}
       {showDetails && (
         
-        <div className={styles.details}>
-          <br/>
-          <p className={styles.cell}><strong>Pronúncia: falada e fonética</strong></p>
-          <div className={styles.container}>
-            <p className={styles.cell}> {verb.infinitive}</p>
-            <p className={styles.cell}> {verb.simple_past}</p>
-            <p className={styles.cell}> {verb.past_participle}</p>
-          </div>
-          <div className={styles.container}>
-            <p className={styles.cell}> {verb.spoken_infinitive}</p>
-            <p className={styles.cell}> {verb.spoken_simple_past}</p>
-            <p className={styles.cell}> {verb.spoken_past_participle}</p>
-          </div>
-          <div className={styles.container}>
-            <p className={styles.cell}> {verb.phonetic_infinitive}</p>
-            <p className={styles.cell}> {verb.phonetic_simple_past}</p>
-            <p className={styles.cell}> {verb.phonetic_past_participle}</p>
-          </div>
-          <br/>
-          <p className={styles.cell}><strong>Exemplo:</strong></p>
-          <div>
-            <p className={styles.cell}><strong>Infinitive:</strong> {verb.exemple_infinitive}</p>
-            <p className={styles.cell}><strong>Simple Past:</strong> {verb.exemple_simple_past}</p>
-            <p className={styles.cell}><strong>Past participle:</strong> {verb.exemple_past_participle}</p>
-          </div>
-          <br/>
+      <div className={styles.details}>
+        <div>
+          <p className={styles.detailsTitle}><strong>Pronúncia: falada e fonética</strong> </p>
         </div>
+        <div className={styles.container}>
+          <div className={styles.detailsVerb}>
+            <p className={styles.detailsCell}> {verb.infinitive} </p>
+            {/* <button className={styles.detailsButton}><GrEdit /></button> */}
+            <button className={styles.detailsButton}><GrPlay /></button>
+          </div>
+          <div className={styles.detailsVerb}>
+            <p className={styles.detailsCell}> {verb.simple_past} </p>
+            {/* <button className={styles.detailsButton}><GrEdit /></button> */}
+            <button className={styles.detailsButton}><GrPlay /></button>
+          </div>
+          <div className={styles.detailsVerb}>
+            <p className={styles.detailsCell}> {verb.past_participle} </p>
+            {/* <button className={styles.detailsButton}><GrEdit /></button> */}
+            <button className={styles.detailsButton}><GrPlay /></button>
+          </div>
+        </div>
+        <div className={styles.container}>
+          <div className={styles.detailsVerb}>
+            <p className={styles.detailsCell}> {verb.spoken_infinitive} </p>
+            <button className={styles.detailsButton}><GrEdit /></button>
+          </div>
+          <div className={styles.detailsVerb}>
+            <p className={styles.detailsCell}> {verb.spoken_simple_past} </p>
+            <button className={styles.detailsButton}><GrEdit /></button>
+          </div>
+          <div className={styles.detailsVerb}>
+            <p className={styles.detailsCell}> {verb.spoken_past_participle} </p>
+            <button className={styles.detailsButton}><GrEdit /></button>
+          </div>
+        </div>
+        <div className={styles.container}>
+          <div className={styles.detailsVerb}>
+            <p className={styles.detailsCell}> {verb.phonetic_infinitive} </p>
+            {/* <button className={styles.detailsButton}><GrEdit /></button> */}
+          </div>
+          <div className={styles.detailsVerb}>
+            <p className={styles.detailsCell}> {verb.phonetic_simple_past} </p>
+            {/* <button className={styles.detailsButton}><GrEdit /></button> */}
+          </div>
+          <div className={styles.detailsVerb}>
+             <p className={styles.detailsCell}> {verb.phonetic_past_participle} </p>
+            {/* <button className={styles.detailsButton}><GrEdit /></button> */}
+          </div>
+        </div>          
+        <div>
+          <p className={styles.detailsTitle}><strong>Exemplos</strong> </p>
+        </div>
+        <div>
+          <div className={styles.detailsExamples}>
+           <p className={styles.detailsCell}><strong>Infinitive:</strong> {verb.exemple_infinitive} </p>
+           <button className={styles.detailsButton}><GrEdit /></button>
+           <button className={styles.detailsButton}><GrPlay /></button>
+          </div> 
+            <div className={styles.detailsExamples}>
+              <p className={styles.detailsCell}><strong>Simple Past:</strong> {verb.exemple_simple_past} </p>
+              <button className={styles.detailsButton}><GrEdit /></button>
+              <button className={styles.detailsButton}><GrPlay /></button>
+            </div> 
+            <div className={styles.detailsExamples}>
+              <p className={styles.detailsCell}><strong>Past participle:</strong> {verb.exemple_past_participle} </p>
+              <button className={styles.detailsButton}><GrEdit /></button>
+              <button className={styles.detailsButton}><GrPlay /></button>            
+            </div>            
+          </div>
+        <br/>
+      </div>
       )}
     </div>
   );
